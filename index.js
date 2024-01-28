09b0f8187ff2159b503d40a0913b38c46f5851ab
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-01-28 09:07:41 -0800

    added more images

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url,left,bottom,) {
+    let image = document.createElement('img')
+image.src = url
+image.style.position = 'fixed'
+image.style.left = left + 'px'
+image.style.bottom = bottom + 'px'
+document.body.append(image)
+return image
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = './assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+newImage('assets/green-character.gif', 100, 100)
+
+newImage('assets/pine-tree.png', 450, 200)
+
+newImage('assets/tree.png', 350,300)
+
+newImage('assets/pillar.png', 350, 100)
+
+newImage('assets/crate.png', 150, 200)
+
+newImage('assets/well.png', 500, 425)
