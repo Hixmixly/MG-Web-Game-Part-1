07940f9cdcff1e644184ba43141a202d706f80f6
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-01-29 20:01:56 -0800

    added items and dbclck

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function newImage(url,left,bottom,) {
     let image = document.createElement('img')
-image.src = url
-image.style.position = 'fixed'
-image.style.left = left + 'px'
-image.style.bottom = bottom + 'px'
-document.body.append(image)
-return image
+    image.src = url
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image
 }
 
 newImage('assets/green-character.gif', 100, 100)
@@ -19,3 +19,19 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 
 newImage('assets/well.png', 500, 425)
+
+function newItem(url, left, bottom){
+   let image = newImage(url,left,bottom)
+
+   image.addEventListener('dblclick', () => {
+    image.remove()
+})
+}
+
+
+
+newItem('assets/sword.png',500, 405 )
+
+newItem('assets/shield.png', 165, 185)
+
+newItem('assets/staff.png', 600, 100)
